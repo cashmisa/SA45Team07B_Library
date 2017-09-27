d12d22822a81c10db1101b7e35383389632b55cd
CK Tan <tanck105@hotmail.com>
2017-09-27 12:58:32 +0800

    made changes to base form links

--- SA45Team07B/BaseForm.cs
+++ SA45Team07B/BaseForm.cs
@@ -32,9 +32,101 @@ namespace SA45Team07B
             this.WindowState = FormWindowState.Normal;
         }
 
-        private void addNewToolStripMenuItem2_Click(object sender, EventArgs e)
+
+        private void borrowToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            
+            BorrowForm borrowform = new BorrowForm();
+            borrowform.ShowDialog();
+        }
+
+        private void returnToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ReturnForm returnform = new ReturnForm();
+            returnform.ShowDialog();
+        }
+
+        private void recordLossToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            RecordLost recordlost = new RecordLost();
+            recordlost.ShowDialog();
+        }
+
+        private void adjustStockToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AdjustStock adjuststock = new AdjustStock();
+            adjuststock.ShowDialog();
+        }
+
+        private void bookToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            BookInfo bookinfo = new BookInfo();
+            bookinfo.ShowDialog();
+        }
+
+        private void memberToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MemberPopUpSearch mempopupsearch = new MemberPopUpSearch();
+            mempopupsearch.ShowDialog();
+        }
+
+        private void addNewToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddNewBook addnewbook = new AddNewBook();
+            addnewbook.ShowDialog();
+        }
+
+        private void modifyInformationToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ModifyBook modifybook = new ModifyBook();
+            modifybook.ShowDialog();
+        }
+
+        private void addNewToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            AddNewMember addnewmember = new AddNewMember();
+            addnewmember.ShowDialog();
+        }
+
+        private void modifyInformationToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            ModifyMember modifymember = new ModifyMember();
+            modifymember.ShowDialog();
+        }
+
+        private void publisherToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddNewPublisher addnewpublisher = new AddNewPublisher();
+            addnewpublisher.ShowDialog();
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AboutBox aboutbox = new AboutBox();
+            aboutbox.ShowDialog();
+
+        }
+
+        //private void creditsToolStripMenuItem_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        private void toolStripButtonBorrow_Click(object sender, EventArgs e)
+        {
+            BorrowForm borrowform = new BorrowForm();
+            borrowform.ShowDialog();
+        }
+
+        private void toolStripButtonReturn_Click(object sender, EventArgs e)
+        {
+            ReturnForm returnform = new ReturnForm();
+            returnform.ShowDialog();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            BookInfo bookinfo = new BookInfo();
+            bookinfo.ShowDialog();
         }
     }
 }
